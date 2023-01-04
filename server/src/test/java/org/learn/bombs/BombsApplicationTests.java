a163baf5828585fbe87b85299da869430f984b69
LearningStacy <stacy.saintpaul@gmail.com>
2023-01-04 12:25:22 -0500

    setup for foundation

---
+++ server/src/test/java/org/learn/bombs/BombsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learn.bombs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BombsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
