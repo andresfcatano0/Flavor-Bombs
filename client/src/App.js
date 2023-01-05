9c33657f32f74ca24143b244755a63b2f70e7cbf
LearningStacy <stacy.saintpaul@gmail.com>
2023-01-05 05:56:32 -0500

    create routes for navbar to work

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import AuthNavBar from './components/navbar/AuthNavBar';
+import AdminNavBar from './components/navbar/AdminNavBar';
+import NavBar from './components/navbar/NavBar';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <BrowserRouter>
+      <NavBar/>
+      <Switch>
+        <Route exact path="/">
+          <HomePage/>
+        </Route>
+        <Route path="/login">
+          <LoginPage/>
+        </Route>  
+
+      </Switch>
+      {/* <AdminNavBar/> */}
+    </BrowserRouter>
   );
 }
 
