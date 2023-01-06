51cec2195c36e3fcb1d896679744d5feb3311e6d
LearningStacy <stacy.saintpaul@gmail.com>
2023-01-06 14:50:15 -0500

    update api and update card info

--- client/src/components/RestaurantCard.js
+++ client/src/components/RestaurantCard.js
@@ -12,7 +12,7 @@ export default function RestaurantCard() {
     const [isLoading, setIsLoading] = useState(true);
 
     const getRestaurants = async () => {
-        await fetch("http://localhost:8080/api/restaurant/public", {
+        await fetch("http://localhost:8080/api/restaurant", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
@@ -26,10 +26,10 @@ export default function RestaurantCard() {
             console.log(data);
             setRestaurants(data);
           })
-          .catch((err) => {
-            setIsLoading(false);
-            console.log(err);
-          });
+           .catch((err) => {
+             setIsLoading(false);
+             console.log(err);
+           });
     }
 
     useEffect(() => {
@@ -38,30 +38,7 @@ export default function RestaurantCard() {
 
 
   return (
-    // <Card style={{ width: "28rem" }}>
     <>
-      {/* <Card.Img
-        variant="top"
-        src="https://cdn.pixabay.com/photo/2018/07/14/15/27/cafe-3537801_1280.jpg"
-      />
-      <div style={{ textAlign: "end", marginRight: "1rem" }}>
-        <Card.Title className="justify-content-end">Restaurant Name</Card.Title>
-        <Card.Subtitle className="text-muted">
-          Hours: 8 AM - 7PM <br />
-          Sat - Tues
-        </Card.Subtitle>
-      </div>
-      <Card.Body>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <div className="d-grid gap-2 white-text">
-            <Button href="/restaurant-information" variant="primary" size="lg">
-                More Info
-            </Button>
-        </div>
-    </Card.Body> */}
       <Container>
         <Row>
           {isLoading ? (
@@ -84,15 +61,12 @@ export default function RestaurantCard() {
                         {restaurant.restaurantName}
                       </Card.Title>
                       <Card.Subtitle className="text-muted">
-                        Hours: 8 AM - 7PM <br />
-                        Sat - Tues
+                        {restaurant.openHours}
+                        {/* {restaurant.openHours.replace("-", "\n\n")} */}
                       </Card.Subtitle>
                     </div>
                     <Card.Body>
-                      <Card.Text>
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
-                      </Card.Text>
+                      <Card.Text>{restaurant.description}</Card.Text>
                       <div className="d-grid gap-2 white-text">
                         <Button variant="primary" size="lg">
                           More Info
