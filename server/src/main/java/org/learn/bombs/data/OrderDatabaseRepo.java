e445f76bd9220686beb1724fbdd7bf0b619d700c
Andres Catano <andresfcatano0@gmail.com>
2023-01-06 12:06:36 -0500

    connect view order functionality to database

---
+++ server/src/main/java/org/learn/bombs/data/OrderDatabaseRepo.java
@@ -0,0 +1,32 @@
+package org.learn.bombs.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import org.learn.bombs.models.Order;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+
+@Repository
+public class OrderDatabaseRepo implements OrderRepo{
+
+    @Autowired
+    JdbcTemplate template;
+
+    @Override
+    public List<Order> getPrivateOrdersByUsername(String username) {
+        String sql = "select orders.* \n" +
+                "from orders\n" +
+                "inner join app_user on orders.app_user_id = app_user.app_user_id\n" +
+                "where username = ?";
+
+        List<Order> personalOrders = template.query(sql, new OrderMapper(), username);
+
+        return personalOrders;
+    }
+
+}
