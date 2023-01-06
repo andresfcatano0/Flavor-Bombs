caa8f6417828f41b26b6dbb5c27cd7c34c47b791
LearningStacy <stacy.saintpaul@gmail.com>
2023-01-06 07:23:39 -0500

    add loader condition for async/await

--- client/src/components/RestaurantCard.js
+++ client/src/components/RestaurantCard.js
@@ -1,27 +1,35 @@
 import React, {useEffect, useState} from 'react'
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import SkeletonLoader from './SkeletonLoader';
 
 export default function RestaurantCard() {
     const [restaurants, setRestaurants] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const getRestaurants = async () => {
+        await fetch("http://localhost:8080/api/restaurant/public", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((res) => {
+              setIsLoading(false);
+            return res.json();
+          })
+          .then((data) => {
+            console.log(data);
+            setRestaurants(data);
+          })
+          .catch((err) => {
+            setIsLoading(false);
+            console.log(err);
+          });
+    }
 
     useEffect(() => {
-      fetch("http://localhost:8080/api/restaurant/public", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setRestaurants(data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      getRestaurants()
     }, []);
 
 
@@ -50,33 +58,35 @@ export default function RestaurantCard() {
             </Button>
         </div>
       </Card.Body> */}
-      {restaurants.map((restaurant)=> {
+      { isLoading ? <SkeletonLoader/> : 
+      
+      restaurants.map((restaurant)=> {
         return (
             <Card key={restaurant.restaurantId} style={{ width: "28rem" }}>
-      <Card.Img
-        variant="top"
-        src="https://cdn.pixabay.com/photo/2018/07/14/15/27/cafe-3537801_1280.jpg"
-      />
-      <div style={{ textAlign: "end", marginRight: "1rem" }}>
-        <Card.Title className="justify-content-end">{restaurant.restaurantName}</Card.Title>
-        <Card.Subtitle className="text-muted">
-          Hours: 8 AM - 7PM <br />
-          Sat - Tues
-        </Card.Subtitle>
-            
-      </div>
-      <Card.Body>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <div className="d-grid gap-2 white-text">
-            <Button variant="primary" size="lg">
-                More Info
-            </Button>
-        </div>
-      </Card.Body> 
-    </Card>
+                <Card.Img
+                    variant="top"
+                    src="https://cdn.pixabay.com/photo/2018/07/14/15/27/cafe-3537801_1280.jpg"
+                />
+                <div style={{ textAlign: "end", marginRight: "1rem" }}>
+                    <Card.Title className="justify-content-end">{restaurant.restaurantName}</Card.Title>
+                    <Card.Subtitle className="text-muted">
+                    Hours: 8 AM - 7PM <br />
+                    Sat - Tues
+                    </Card.Subtitle>
+                        
+                </div>
+                <Card.Body>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <div className="d-grid gap-2 white-text">
+                        <Button variant="primary" size="lg">
+                            More Info
+                        </Button>
+                    </div>
+                </Card.Body> 
+            </Card>
         )
       })
     }
