0530b6d82b4e1b2e4eff679c27f181ce80a1b675
LearningStacy <stacy.saintpaul@gmail.com>
2023-01-08 09:13:25 -0500

    BUG: cannot delete as admin

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -12,73 +12,27 @@ import Modal from "react-bootstrap/Modal";
 import { MenuDown, PeopleFill, Shop, Trash3 } from 'react-bootstrap-icons';
 
 
-export default function AdminPage({ restaurants, getMenuByRestaurant, menu }) {
+
+export default function AdminPage({ restaurants}) {
   const adminUser = useContext(UserContext);
-  const [modalShow, setModalShow] = useState(false);
-  const [newMenu, setNewMenu] = useState([...menu]);
 
-  const [isLoading, setIsLoading] = useState(true);
+    const deleteRestaurant = (restaurantId) => {
+        if(window.confirm("Are you sure you want to delete this restaurant?")){
 
-  const handleMenuModal = (restaurantId) => {
-    let savedRestaurant = restaurants.filter(r=> r.restaurantId == restaurantId);
-    console.log(savedRestaurant[0].restaurantName)
-    
-    getMenuByRestaurant(restaurantId)
-    console.log(newMenu)
+            fetch("http://localhost:8080/api/restaurant/"+restaurantId, {
+                method: 'DELETE',
+                headers: {
+                    "Authorization": "Bearer " + adminUser.token
+                }
+            }).then(res=>{
+                return res.json()
+            }).then(data => {
+                console.log(data.statusCode)
+            })
+        }
+        }
 
-    
-    
-    // setNewMenu(getMenuByRestaurant(restaurantId));
-    // setIsLoading(false)
-    // setModalShow(true);
-    
-  };
-
-
-//   function MyVerticallyCenteredModal(props) {
-//     // let restaurantMenu = {
-//     //     menuId: "", 
-//     //     itemName: "",
-//     //     itemDescription: "",
-//     //     itemPrice: "", 
-//     //     restaurantId:""
-//     // }
-
-//     // console.log(menu)
-
-//     return (
-//         // isLoading ? (
-//         //     <p>Loading..</p>
-//         // ):(
-//       <Modal
-//         {...props}
-//         size="lg"
-//         aria-labelledby="contained-modal-title-vcenter"
-//         centered
-//       >
-//         <Modal.Header closeButton>
-//           <Modal.Title id="contained-modal-title-vcenter">
-            
-//           </Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>
-//           <h4>Centered Modal</h4>
-//           <p>
-//             Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-//             dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-//             ac consectetur ac, vestibulum at eros.
-//           </p>
-//         </Modal.Body>
-//         <Modal.Footer>
-//           <Button onClick={props.onHide}>Close</Button>
-//         </Modal.Footer>
-//       </Modal>
-//     //   )
-//     );
-//   }
-
-  
-
+ 
   return (
     <>
       <Container className="d-flex mt-4">
@@ -116,6 +70,8 @@ export default function AdminPage({ restaurants, getMenuByRestaurant, menu }) {
                         <tr>
                           <th></th>
                           <th>Restaurant Name</th>
+                          <th>Restaurant Address</th>
+                          <th>Description</th>
                           <th>Actions</th>
                         </tr>
                       </thead>
@@ -125,17 +81,13 @@ export default function AdminPage({ restaurants, getMenuByRestaurant, menu }) {
                             <tr key={index}>
                               <td>{index + 1}</td>
                               <td>{restaurant.restaurantName}</td>
+                              <td>{restaurant.address}</td>
+                              <td>{restaurant.description}</td>
                               <td className="d-flex justify-content-around">
-                                <Button
-                                  onClick={() => {
-                                    handleMenuModal(restaurant.restaurantId);
-                                  }}
-                                  className="btn btn-dark d-flex align-items-center"
-                                >
-                                  <span className="px-2">View Menu</span>
-                                  <MenuDown />
-                                </Button>
-                                <Button className="btn btn-danger d-flex align-items-center">
+                               
+                                <Button onClick={()=>{
+                                    deleteRestaurant(restaurant.restaurantId);
+                                }} className="btn btn-danger d-flex align-items-center">
                                   <span className="px-2">Delete</span>
                                   <Trash3 />
                                 </Button>
@@ -153,37 +105,11 @@ export default function AdminPage({ restaurants, getMenuByRestaurant, menu }) {
             </Row>
           </Tab.Container>
         </Col>
-        {/* <Col>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th></th>
-                <th>Restaurant Name</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {restaurants.map((restaurant, index) => {
-                return (
-                  <tr key={index}>
-                    <td>{index + 1}</td>
-                    <td>{restaurant.restaurantName}</td>
-                    <td>
-                      <Button className="btn btn-success">Edit</Button>
-                      <Button className="btn btn-danger">Delete</Button>
-                    </td>
-                  </tr>
-                );
-              })}
-              <tr></tr>
-            </tbody>
-          </Table>
-        </Col> */}
+        
       </Container>
-      {/* <MyVerticallyCenteredModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      /> */}
+   
+
+      
     </>
   );
 }
