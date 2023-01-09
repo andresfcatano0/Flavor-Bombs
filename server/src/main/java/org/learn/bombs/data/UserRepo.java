e50e87cab1fde2704ca25f49b51699efd9b1ad07
Andres Catano <andresfcatano0@gmail.com>
2023-01-09 14:34:39 -0500

    connect viewing AppUser functionality to database

--- server/src/main/java/org/learn/bombs/data/UserRepo.java
+++ server/src/main/java/org/learn/bombs/data/UserRepo.java
@@ -2,6 +2,14 @@ package org.learn.bombs.data;
 
 import org.learn.bombs.models.AppUser;
 
+import java.util.List;
+
 public interface UserRepo {
+
     AppUser loadUserByUsername(String username);
+
+    List<AppUser> getAllAppUsers();
+
+    AppUser getAppUserById(Integer id);
+
 }
