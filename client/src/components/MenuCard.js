49bf48959c2d5fb05520dc720e8f5587b7fdc360
LearningStacy <stacy.saintpaul@gmail.com>
2023-01-10 10:46:58 -0500

    dynamically cart items to cart

--- client/src/components/MenuCard.js
+++ client/src/components/MenuCard.js
@@ -4,7 +4,29 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 
-export default function MenuCard({m}) {
+import CartContext from "../context/cart/CartContext";
+import { useContext } from 'react';
+import { PlusCircle } from 'react-bootstrap-icons';
+
+export default function MenuCard({
+  m,
+  
+}) {
+  const {
+    addItemToCart,
+    increaseQuantity,
+
+    orderCartItems,
+    handleTotals,
+    itemCount,
+  } = useContext(CartContext);
+
+  const isItemInCart = (m) => {
+    return orderCartItems?.find((item) => item.menuId === m.menuId);
+  };
+
+  console.log(orderCartItems);
+
   return (
     <Container
       className="mb-4 pe-5"
@@ -25,11 +47,45 @@ export default function MenuCard({m}) {
         </Col>
         <Col className="d-flex">
           <Row>
-            <h5 className="mt-3 ">{m.itemName}</h5>
+            <h5 className="mt-3 ">
+              {m.itemName} - <span>${m.itemPrice}</span>
+            </h5>
             <p>{m.itemDescription}</p>
           </Row>
           <Row className="align-items-end mb-3">
-            <Button className="">Order Now</Button>
+            {/* {isItemInCart(m) && (
+              <PlusCircle
+                onClick={() => {
+                  increaseQuantity(m);
+                }}
+              />
+            )}
+          */}
+
+            {/* <Button className="">Order Now</Button> */}
+            {isItemInCart(m) && (
+              <Button
+                onClick={() => {
+                  increaseQuantity(m);
+                }}
+              >
+                <PlusCircle />
+                
+                {m.quantity}
+              </Button>
+            )}
+
+            {/* If item is not in cart show order now button then change to other button above */}
+            {!isItemInCart(m) && (
+              <Button
+                onClick={() => {
+                  addItemToCart(m);
+                }}
+              >
+                Order Now
+              </Button>
+            )}
+            {console.log(handleTotals)}
           </Row>
         </Col>
       </Row>
