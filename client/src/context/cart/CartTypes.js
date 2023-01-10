761e45da30487d1962390f8626642e4fde1209f8
LearningStacy <stacy.saintpaul@gmail.com>
2023-01-10 09:47:12 -0500

    define different  cart actions

---
+++ client/src/context/cart/CartTypes.js
@@ -0,0 +1,6 @@
+export const ADD_ITEM_TO_CART = "ADD_ITEM_TO_CART";
+export const REMOVE_ITEM_FROM_CART = "REMOVE_ITEM_FROM_CART";
+export const INCREASE = "INCREASE";
+export const DECREASE = "DECREASE";
+export const CHECKOUT_ORDER = "CHECKOUT_ORDER";
+export const CLEAR = "CLEAR";
