f9c16dc6557020845a9802a65ef5199a27770ee4
LearningStacy <stacy.saintpaul@gmail.com>
2023-01-10 10:45:04 -0500

    add cart state for all if the app

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
 import App from './App';
+import CartState from './context/cart/CartState';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CartState>
+
+     <App />
+    </CartState>
   </React.StrictMode>
 );
