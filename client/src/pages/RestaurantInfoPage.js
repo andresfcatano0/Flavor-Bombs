5d6679e1bfcb55640b669454eca5002e2d37904c
LearningStacy <stacy.saintpaul@gmail.com>
2023-01-11 13:39:32 -0500

    fetched data for specific restaurants

--- client/src/pages/RestaurantInfoPage.js
+++ client/src/pages/RestaurantInfoPage.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Carousel from 'react-bootstrap/Carousel';
@@ -9,129 +10,123 @@ import { useParams } from "react-router-dom";
 import ReviewCard from '../components/ReviewCard';
 import MenuCard from '../components/MenuCard';
 import CartContext from '../context/cart/CartContext';
+import { DashCircle, PlusCircle, PlusCircleFill, Trash } from 'react-bootstrap-icons';
 
-export default function RestaurantInfoPage({getRestaurants, restaurants}) {
-    // const [index, setIndex] = useState(0);
+export default function RestaurantInfoPage({ restaurants, getRestaurants, menus }) {
+  // const [index, setIndex] = useState(0);
 
-    // const handleSelect = (selectedIndex, e) => {
-    //   setIndex(selectedIndex);
-    // };
+  // const handleSelect = (selectedIndex, e) => {
+  //   setIndex(selectedIndex);
+  // };
 
-    
+  const [menu, setMenu] = useState([]);
+  const params = useParams();
+  const [specificRestaurant, setSpecificRestaurant] = useState({});
 
-
-
-    const [menu, setMenu] = useState([]);
-    const params = useParams();
-    const [specificRestaurant, setSpecificRestaurant] = useState({});
-
-    const getMenuByRestaurant = () => {
-        fetch("http://localhost:8080/api/menu", {
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }).then(response => {
-            return response.json();
-        }).then(data => {
-            let specificMenu = data.filter(d=> {
-                return d.restaurantId.toString() === params.id.toString()
-            })
-            // console.log(specificMenu);
-            setMenu(specificMenu);
-        }).catch(err=> console.log(err));
-    }
-
-
-const {
-  addItemToCart,
-  increaseQuantity,
-
-  orderCartItems,
-  handleTotals,
-  itemCount,
-} = useContext(CartContext);
-
-const itemInCart = (menuItems) => {
-  return orderCartItems?.find((item) => item.menuId === menuItems.menuId);
-};
-
-
-
-
-
-
-
-
-const getSpecificRestaurant = () => {
-  fetch(`http://localhost:8080/api/restaurant/${params.id}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-      
-    },
+  let found = menus.filter(m => {
+    return (m.restaurantId == params.id)
   })
-  .then(res=> {
-    return res.json();
-  })
-  .then(data => {
-    // console.log(data)
-    setSpecificRestaurant(data);
-  })
-}
 
-const [reviews, setReviews] = useState([]);
-const getReviewPerRestaurant = () => {
-  fetch("http://localhost:8080/api/review", {
-    method: 'GET',
-    headers:{
-      "Content-Type": "application/json"
-    }
-  }).then(res=> {
-    return res.json();
-  }).then(data=> {
-    // console.log(data)
-    // for(let i = 0; i<data.length;i++){
-    //   if (data[i].restaurantId === params.id) {
-    //     setReviews(data[i]);
-    //   }
-    // }
-    let specificReviews = data.filter(d=> {
-      return d.restaurantId.toString() === params.id.toString()
+  const {
+    addItemToCart,
+    increaseQuantity,
+    decreaseQuantity,
+    removeItemFromCart,
+    orderCartItems
+  } = useContext(CartContext);
+
+  // return orderCartItems?.find((item) => item.menuId === menuItems.menuId);
+  const isItemInCart = (menuItems) => {
+    return orderCartItems.find((item) => item.menuId == menuItems.menuId) !== undefined;
+  };
+
+  const getSpecificRestaurant = () => {
+    fetch(`http://localhost:8080/api/restaurant/${params.id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    setReviews(specificReviews)
-    // console.log(specificReviews)
-  })
-}
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data)
+        setSpecificRestaurant(data);
+      });
+  };
 
-useEffect(()=> {
-  getSpecificRestaurant();
-  getMenuByRestaurant();
-  getReviewPerRestaurant();
-  }, [])
+  // const getMenuByRestaurant = async () => {
+  //   await fetch("http://localhost:8080/api/menu", {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   })
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       let specificMenu = data.filter((d) => {
+  //         return d.restaurantId.toString() == params.id.toString();
+  //       });
+  //       // console.log(data);
+  //       setMenu(specificMenu);
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
+
+  const [reviews, setReviews] = useState([]);
+  const getReviewPerRestaurant = () => {
+    fetch("http://localhost:8080/api/review", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data)
+        // for(let i = 0; i<data.length;i++){
+        //   if (data[i].restaurantId === params.id) {
+        //     setReviews(data[i]);
+        //   }
+        // }
+        let specificReviews = data.filter((d) => {
+          return d.restaurantId.toString() === params.id.toString();
+        });
+        setReviews(specificReviews);
+        // console.log(specificReviews)
+      });
+  };
+
   
-    return (
-      // <div className="mt-4 flex-column text-center">
-      <>
-        <div className="mt-4" key={specificRestaurant.restaurantId}>
-          <div className="text-center">
-            <img
-              className="rounded"
-              height={"350px"}
-              src="https://cdn.pixabay.com/photo/2018/07/14/15/27/cafe-3537801_1280.jpg"
-            />
-          </div>
-          <div>
-            <h3 className="mt-2 text-end">
-              {specificRestaurant.restaurantName}
-            </h3>
-          </div>
-          <div className="text-end">
-            <p className="text-muted">{specificRestaurant.address}</p>
-            <p className="text-muted">{specificRestaurant.openHours}</p>
-          </div>
-          <hr />
-          {/* <Carousel activeIndex={index} onSelect={handleSelect}>
+
+  useEffect(() => {
+    getSpecificRestaurant();
+    // getMenuByRestaurant();
+    getReviewPerRestaurant();
+  }, []);
+
+  return (
+    // <div className="mt-4 flex-column text-center">
+    <>
+      <div className="mt-4 mx-4" key={specificRestaurant.restaurantId}>
+        <div className="text-center">
+          <img src={specificRestaurant.restaurantImage} />
+          
+        </div>
+        <div>
+          <h3 className="mt-2 text-end">{specificRestaurant.restaurantName}</h3>
+        </div>
+        <div className="text-end">
+          <p className="text-muted">{specificRestaurant.address}</p>
+          <p className="text-muted">{specificRestaurant.openHours}</p>
+        </div>
+        <hr />
+        {/* <Carousel activeIndex={index} onSelect={handleSelect}>
               <Carousel.Item>
                 <ReviewCard/>
               </Carousel.Item>
@@ -142,26 +137,116 @@ useEffect(()=> {
                 <ReviewCard/>
               </Carousel.Item>
             </Carousel> */}
-          <Row>
-            <ReviewCard reviews={reviews}/>
-            {console.log(reviews)}
-            {/* <ReviewCard /> */}
-          </Row>
-          <hr />
-          <div>
-            <h4>Menu</h4>
-          </div>
-          {menu.map(m=>{
+        <Row>
+          <ReviewCard reviews={reviews} />
+          {/* {console.log(reviews)} */}
+          {/* <ReviewCard /> */}
+        </Row>
+        <hr />
+        <div>
+          <h4>Menu</h4>
+          {/* </div>
+        {menu.map((m) => {
+          return <MenuCard key={m.menuId} m={m} />;
+        })} */}
+          {console.log(found)}
+          
+          {found.map((m) => {
             return (
-                <MenuCard 
-                key={m.menuId} 
-                m={m} 
+              <Container
+                key={m.menuId}
+                className="mb-4 pe-5"
+                style={{
+                  backgroundColor: "lightGrey",
+                  width: "90vw",
+                  borderRadius: "1rem",
+                }}
+              >
+                <Row>
+                  <Col>
+                    <img
+                      width={"350px"}
+                      style={{ borderRadius: "1.7rem" }}
+                      className="p-2, my-3"
+                      // src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_1280.jpg"
+                      src={m.itemImage}
+                    />
+                  </Col>
+                  <Col className="d-flex">
+                    <Row>
+                      <h5 className="mt-3 ">
+                        {m.itemName} - <span>${m.itemPrice}</span>
+                      </h5>
+                      <p>{m.itemDescription}</p>
+                    </Row>
+                    <Row className="align-items-end mb-3">
+                      {/* {isItemInCart(m) && (
+                        <Button
+                          onClick={() => {
+                            increaseQuantity(m);
+                          }}
+                        >
+                          <PlusCircle /> Add More
+                        </Button>
+                      )} */}
 
-                
-                />
-            )
+                      
+                      
+                      {isItemInCart(m) && (
+                        <>
+                          <Button
+                            onClick={() => {
+                              increaseQuantity(m);
+                            }}
+                          >
+                            <PlusCircleFill />
+                          </Button>
+                        
+                      
+                         
+                        </>
+                      )
+                      }
+
+                      {/* <Button
+                        className="btn-success"
+                        onClick={() => increaseQuantity(item)}
+                      >
+                        <PlusCircleFill />
+                      </Button>
+                      <span>{item.quantity}</span> */}
+
+                      
+                      {/* <Button className="">Order Now</Button> */}
+                      {!isItemInCart(m) && (
+                        <Button
+                          onClick={() => {
+                            addItemToCart(m);
+                          }}
+                        >
+                          Order Now
+                        </Button>
+                      )}
+
+                      {/* If item is not in cart show order now button then change to other button above */}
+                      {/* {!isItemInCart(m) && (
+                        <Button
+                          onClick={() => {
+                            addItemToCart(m);
+                          }}
+                        >
+                          Order Now
+                        </Button>
+                      )}
+                      {console.log(handleTotals)} */}
+                    </Row>
+                  </Col>
+                </Row>
+              </Container>
+            );
           })}
         </div>
-      </>
-    );
+      </div>
+    </>
+  );
 }
