765b7c7e001b8d2eac3738bdda98aa9a6d613c1f
Andres Catano <andresfcatano0@gmail.com>
2023-01-12 12:25:57 -0500

    complete restaurant data layer tests

--- server/src/test/java/org/learn/bombs/data/RestaurantDatabaseRepoTest.java
+++ server/src/test/java/org/learn/bombs/data/RestaurantDatabaseRepoTest.java
@@ -1,21 +1,69 @@
 package org.learn.bombs.data;
 
+import org.learn.bombs.models.Restaurant;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class RestaurantDatabaseRepoTest {
 
-    @Test
-    void getPublicRestaurants() {
+    @Autowired
+    RestaurantDatabaseRepo repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
     }
 
     @Test
-    void deleteRestaurantById() {
+    void shouldFindAllRestaurants() {
+        List<Restaurant> restaurants = repository.getPublicRestaurants();
+        assertNotNull(restaurants);
+        assertEquals(3, restaurants.size());
     }
 
     @Test
-    void getRestaurantById() {
+    void shouldFindRestaurantById() {
+        Restaurant restaurant = repository.getRestaurantById(1);
+        assertEquals(1, restaurant.getRestaurantId());
+        assertEquals("Taco House", restaurant.getRestaurantName());
+        assertEquals("Serving the best tacos in town since 1958", restaurant.getDescription());
     }
+
+    @Test
+    void shouldNotFindRestaurantById() {
+        Restaurant restaurant = repository.getRestaurantById(99);
+        assertNull(restaurant);
+    }
+
+    @Test
+    void shouldDeleteRestaurant() {
+        repository.deleteRestaurantById(1);
+        Restaurant restaurant = repository.getRestaurantById(1);
+        assertNull(restaurant);
+
+        List<Restaurant> restaurants = repository.getPublicRestaurants();
+        assertEquals(2, restaurants.size());
+    }
+
+    @Test
+    void shouldNotDeleteRestaurant() {
+        repository.deleteRestaurantById(99);
+        Restaurant restaurant = repository.getRestaurantById(99);
+        assertNull(restaurant);
+
+        List<Restaurant> restaurants = repository.getPublicRestaurants();
+        assertEquals(3, restaurants.size());
+    }
+
 }
 
