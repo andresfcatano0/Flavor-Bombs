09aaee70f76e704f3bb7565e9cb59f1e67e0b37c
LearningStacy <stacy.saintpaul@gmail.com>
2023-01-15 22:10:54 -0500

    move get all orders to admin file

--- client/src/pages/adminTables/AdminViewAllOrders.js
+++ client/src/pages/adminTables/AdminViewAllOrders.js
@@ -2,48 +2,54 @@ import React, { useContext, useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import Badge from "react-bootstrap/Badge";
-import ListGroup from "react-bootstrap/ListGroup";
-import Card from "react-bootstrap/Card";
-import {
-  Cart2,
-  Clipboard2Data,
-  CreditCardFill,
-  People,
-  PeopleFill,
-  Shop,
-  ShopWindow,
-} from "react-bootstrap-icons";
 import AdminNavLeftPane from "../../components/navbar/AdminNavLeftPane";
 import AdminViewOrderTable from "../AdminViewOrderTable";
 import UserContext from "../../context/AuthContext";
 
-export default function AdminViewAllOrders({allUsers, getAllOrders,allOrders, restaurants,getRestaurants,specificUser,handleSpecificReviewOrder}) {
+export default function AdminViewAllOrders({allUsers, restaurants,getRestaurants,specificUser,handleSpecificReviewOrder}) {
   const adminUser = useContext(UserContext);
 
-   const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-   const handleClose = () => setShow(false);
-   const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-  const deleteOrder = (orderId) => {
-      fetch("http://localhost:8080/api/order/" + orderId, {
-        method: "DELETE",
-        headers: {
-          Authorization: "Bearer " + adminUser.token,
-        },
-      }).then((data) => {
-        // console.log(data);
-        getAllOrders();
-        handleClose()
-        if (data.statusCode === 204) {
-          console.log("successfully deleted order");
-        }
-        // console.log(data.statusCode)
+  // ALL ORDERS
+  const [allOrders, setAllOrders] = useState([]);
+
+  const getAllOrders = async () => {
+    await fetch("http://localhost:8080/api/order/all", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + adminUser?.token,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setAllOrders(data);
       });
   };
 
+  useEffect(()=>{
+    getAllOrders()
+  })
+
+  const deleteOrder = (orderId) => {
+    fetch("http://localhost:8080/api/order/" + orderId, {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + adminUser.token,
+      },
+    }).then((data) => {
+      // console.log(data);
+      getAllOrders();
+      handleClose();
+      if (data.statusCode === 204) {
+        console.log("successfully deleted order");
+      }
+      // console.log(data.statusCode)
+    });
+  };
 
   return (
     <Container fluid className="mt-3">
