496266a4b20495d130cf6beca616c2a06bb9c367
LearningStacy <stacy.saintpaul@gmail.com>
2023-02-24 13:43:58 -0500

    redirect to different page for more personal info

--- client/src/pages/AdminUsersTable.js
+++ client/src/pages/AdminUsersTable.js
@@ -6,8 +6,15 @@ import Modal from "react-bootstrap/Modal";
 import {
   Trash3,
 } from "react-bootstrap-icons";
+import { useHistory } from "react-router-dom";
 
-export default function AdminUsersTable({ handleClose, handleShow, show, allUsers, deleteUser, handleSpecificReviewOrder }) {
+export default function AdminUsersTable({
+  specificUser,handleClose,
+  handleShow,
+  show,
+  allUsers,
+  deleteUser
+}) {
   let userModal = {
     appUserId: 0,
     firstName: "",
@@ -25,11 +32,17 @@ export default function AdminUsersTable({ handleClose, handleShow, show, allUser
     let savedU = {
       appUserId: event.target.value,
       firstName: found[0].firstName,
-      lastName: found[0].lastName
+      lastName: found[0].lastName,
     };
     setUserSavedState(savedU);
     handleShow();
   };
+
+  const history = useHistory();
+  const handleSpecificReviewOrder = (userId) => {
+        history.push("/admin/specific-user/" + userId);
+  };
+
   return (
     <Table striped bordered hover className="text-center">
       <thead>
@@ -53,13 +66,15 @@ export default function AdminUsersTable({ handleClose, handleShow, show, allUser
               <td>{userInfo.email}</td>
 
               <td className="d-flex justify-content-around">
-                {/* <Button
+                <Button
                   value={userInfo.appUserId}
-                  onClick={()=> {
-                    deleteUser(userInfo.appUserId);
+                  onClick={() => {
+                    handleSpecificReviewOrder(userInfo.appUserId);
                   }}
-                  className="btn btn-danger d-flex align-items-center"
-                > */}
+                  className="btn btn-info d-flex align-items-center"
+                >
+                  More Info
+                </Button>
                 <Button
                   value={userInfo.appUserId}
                   onClick={(event) => {
