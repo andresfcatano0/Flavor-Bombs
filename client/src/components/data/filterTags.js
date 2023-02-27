24ef1242f979103dc013ef874beb55d1a31a1f80
LearningStacy <stacy.saintpaul@gmail.com>
2023-02-27 18:03:08 -0500

    created json for filter icons

---
+++ client/src/components/data/filterTags.js
@@ -0,0 +1,44 @@
+const filterMaterial = [
+  {
+    id: 1,
+    img: "filter_tags/fried-chicken.png",
+    textKeyword: "Chicken",
+  },
+  {
+    id: 2,
+    img: "filter_tags/cake-slice.png",
+    textKeyword: "Sweet",
+  },
+  {
+    id: 3,
+    img: "filter_tags/chili.png",
+    textKeyword: "Spicy",
+  },
+  {
+    id: 4,
+    img: "filter_tags/pizza.png",
+    textKeyword: "Pizza",
+  },
+  {
+    id: 5,
+    img: "filter_tags/taco.png",
+    textKeyword: "Taco",
+  },
+  {
+    id: 6,
+    img: "filter_tags/spaguetti.png",
+    textKeyword: "Pasta",
+  },
+  {
+    id: 7,
+    img: "filter_tags/fried-potatoes.png",
+    textKeyword: "Fries",
+  },
+  {
+    id: 8,
+    img: "filter_tags/vegan.png",
+    textKeyword: "Vegan",
+  }
+];
+
+export default filterMaterial;
